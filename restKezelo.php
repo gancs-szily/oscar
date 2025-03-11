02ad460a6bbb7af18997a6edcf932a477c0d028a
gancs-szily <71602533755@szily.hu>
2025-03-11 11:32:19 +0100

    restkezelo.php kész

---
+++ restKezelo.php
@@ -0,0 +1,18 @@
+<?php
+class RestKezelo{
+    private $httpVersion= "HTTP/1.1";
+    public function setHttpFejlec($statuskod){
+        $statusUzenet=$this->gethttpStatusUzenet($statuskod);
+        header($this->httpVersion." ". $statuskod. " ".$statusUzenet);
+        header("Content-Type: Application/json; charset=utf-8");
+    }
+
+    public function gethttpStatusUzenet($statuskod){
+        $httpStatus=array(
+            200=>'OK',
+            400=> 'Bad request',
+            404=> 'Not FOund'
+        );
+        return ($httpStatus[$statuskod]);
+    }
+}
