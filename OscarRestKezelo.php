c687374f6a3e29023269f5320c386ba45653a63c
gancs-szily <71602533755@szily.hu>
2025-03-11 11:45:31 +0100

    oscarrestkezelo.php kész

---
+++ OscarRestKezelo.php
@@ -0,0 +1,45 @@
+<?php
+include("restKezelo.php");
+include("oscar.php");
+
+class OscarRestKezelo extends RestKezelo{
+    function getAllOscars(){
+        $oscars=new Oscar();
+        $sorAdat=$oscars->getAllOscars();
+
+        if(empty($sorAdat)){
+            $statusCode = 404;
+            $sorAdat=array('error'=>"No oscars found");
+        }else {
+            $statusCode=200;
+        }
+
+        $this->sethttpFejlec($statusCode);
+        header('Content-Type: application/json; charset=utf-8');
+        $result['Oscars']=$sorAdat;
+        $response=$this->encodeJson($result);
+        $file_path="OscarsByType.json";
+        $this->printfile($response,$file_path);
+        echo $response;
+    }
+    function getFault(){
+        $statusCode=400;
+        $this->sethttpFejlec($statusCode);
+        header('Content-Type: application/jon; charset=UTF-8');
+        $sorAdat=array('error'=>'Bad request');
+        $result['Fault']=$sorAdat;
+
+        $response=$this->encodeJson($result);
+        $file_path="Fault.json";
+        $this->printfile($response,$file_path);
+        echo $response;
+    }
+
+    function encodeJson($responseData){
+        return json_encode($responseData,JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+
+    function printfile($responseData,$file_path){
+        file_put_contents($file_path,$responseData,LOCK_EX);
+    }
+}
